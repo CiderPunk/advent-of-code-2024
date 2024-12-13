fa15f70ec7fad9f966c739400b2877141f0e12a3
Matt Lander <matthew.lander@emperor.works>
2024-12-13 18:05:22 +0000

    switched back to correct test case

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -198,6 +198,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(164));
+        assert_eq!(result, Some(1206));
     }
 }
